a41fe82e7dd55f3160ec6237cd84ea92559e8a2e
Fabian Guevara <Fabian Guevara@DESKTOP-7K1OQD8>
2021-09-17 19:45:09 -0500

    Primera subida al archivo  al repositorio

---
+++ demo/src/main/java/com/example/demo/Intentonumero50milApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intentonumero50milApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Intentonumero50milApplication.class, args);
+	}
+
+}
